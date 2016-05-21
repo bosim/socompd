71d8314e808c31905ca141719094c514465308f5
Bo Simonsen <bo@geekworld.dk>
2016-05-21 18:44:22 +0200

    Support for internet radio

--- socompd/queue.py
+++ socompd/queue.py
@@ -6,15 +6,18 @@ class Add(mpdserver.Command):
     formatArg=[("uri", mpdserver.OptStr)]
 
     def handle_args(self, uri):
+        if uri.startswith('http://'):
+            uri = uri.replace('http://', 'x-rincon-mp3radio://')
+        if uri.find('#') >= 0:
+            uri = uri[:uri.find('#')]
+
         dev.add_uri_to_queue(uri)
-        self.playlist.playlist_time = 0
 
 mpd.requestHandler.RegisterCommand(Add)
 
 class Clear(mpdserver.Command):
     def handle_args(self):
         dev.clear_queue()
-        self.playlist.playlist_time = 0
 
 mpd.requestHandler.RegisterCommand(Clear)
 
