07683326157e2608ec24c3324d665f5bbed74f09
Bo Simonsen <bo@geekworld.dk>
2016-05-21 23:50:09 +0200

    Some clients may send lsinfo /

--- socompd/collection.py
+++ socompd/collection.py
@@ -10,7 +10,7 @@ class LsInfo(mpdserver.Command):
 
     def toMpdMsg(self):
         result = ""
-        if self.args.get('directory'):
+        if self.args.get('directory', '') and self.args.get('directory', '') != '/':
             for album in dev.get_music_library_information('albums'):
                 argument = self.args.get('directory')
                 if argument.endswith(album.title.encode("utf-8")):
@@ -26,7 +26,7 @@ class LsInfo(mpdserver.Command):
                 dirs.append(album.creator.encode("utf-8") + " - " + album.title.encode("utf-8"))
             dirs.sort()
             
-            result = '\n'.join(['directory: ' + dirname for dirname in dirs])
+            result = '\n'.join(['directory: ' + dirname for dirname in dirs]) + "\n"
 
         return result
 
