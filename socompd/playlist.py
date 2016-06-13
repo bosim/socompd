5031dfdd9b4dee73866475d9037eafad7ec1f9b4
Bo Simonsen <bo@geekworld.dk>
2016-06-13 20:36:06 +0200

    Merged queue.py and playlist.py

--- socompd/playlist.py
+++ socompd/playlist.py
@@ -44,6 +44,23 @@ def playlistInfo(position=None):
             result += songToText(song)
         return result
 
+@mpdCommand("add")
+def add(uri):
+    if uri.startswith('http://'):
+        uri = uri.replace('http://', 'x-rincon-mp3radio://')
+    if uri.find('#') >= 0:
+        uri = uri[:uri.find('#')]
+
+    dev.add_uri_to_queue(uri)
+
+@mpdCommand("addid")
+def addId(uri):
+    return add(uri)
+
+@mpdCommand("clear")
+def clear():
+    dev.clear_queue()
+
 @mpdCommand("delete")
 def delete(id):
     pos = int(id)
